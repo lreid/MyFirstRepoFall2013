2f7309d59e1957f8895450f69e792bf3b5379430
Ben Affleck <lreid@csd.uwo.ca>
2013-08-21 10:20:25 -0400

    first time

---
+++ src/OurGroup.java
@@ -0,0 +1,17 @@
+import java.util.Vector;
+
+public class OurGroup {
+
+private Vector<String> groupMembers;
+public OurGroup()
+{
+groupMembers = new Vector<String>();
+groupMembers.add("Stan Smith");
+groupMembers.add("Bart Simpson");
+groupMembers.add("YOUR NAME"); //PUT YOUR NAME HERE
+}
+public Vector<String> getGroupMembers () {
+return groupMembers;
+}
+
+}
